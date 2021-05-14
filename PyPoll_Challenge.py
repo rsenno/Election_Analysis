e86c8ffeb91fdb94e3064089ad2af26849118611
rsenno <82620982+rsenno@users.noreply.github.com>
2021-05-14 15:28:01 -0500

    Python Challenge Deliverable 2

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -8,7 +8,7 @@ import os
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -104,7 +104,7 @@ with open(file_to_save, "w") as txt_file:
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if (votes > largest_turnout_county_count) and (vote_percentage > winning_percentage):
             largest_turnout_county_count = votes
@@ -119,8 +119,9 @@ with open(file_to_save, "w") as txt_file:
     print(largest_turnout_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
+    txt_file.write(largest_turnout_county_summary)
 
-# Reset winning_percentage to 0 since it was used above.
+    # Reset winning_percentage to 0 since it was used above.
     winning_percentage = 0
 
     # Save the final candidate vote count to the text file.
